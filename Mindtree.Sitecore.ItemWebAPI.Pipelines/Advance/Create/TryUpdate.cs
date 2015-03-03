b13142acfb04c4497ed456e56fa2861838affd2a
vishalniit <vishalniit@gmail.com>
2015-03-03 15:30:39 +0000

    Fixed the fields not updated issue

--- Mindtree.Sitecore.ItemWebAPI.Pipelines/Advance/Create/TryUpdate.cs
+++ Mindtree.Sitecore.ItemWebAPI.Pipelines/Advance/Create/TryUpdate.cs
@@ -5,26 +5,27 @@ using System;
 using System.Web;
 namespace Mindtree.ItemWebApi.Pipelines.Advance.Create
 {
-    /// <summary>
-    /// Update class to update item if required.
-    /// </summary>
-	public class TryUpdate : CreateProcessor
-	{
-        /// <summary>
-        /// Function which update the Item after creating it successfully
-        /// </summary>
-        /// <param name="arguments"></param>
-		public override void Process(CreateArgs arguments)
-		{
-			Assert.ArgumentNotNull(arguments, "arguments");
-			System.Web.HttpRequest request = arguments.Context.HttpContext.Request;
-			if (request.Form.AllKeys.Length == 0)
-			{
-				return;
-			}
-			UpdateArgs updateArgs = new UpdateArgs(arguments.Scope);
-			CorePipeline.Run("itemWebApiUpdate", updateArgs);
-			arguments.Result = updateArgs.Result;
-		}
-	}
+    //This should be not be used when doing serialisation based item creation as field values are there in serialised data.
+    ///// <summary>
+    ///// Update class to update item if required.
+    ///// </summary>
+    //public class TryUpdate : CreateProcessor
+    //{
+    //    /// <summary>
+    //    /// Function which update the Item after creating it successfully
+    //    /// </summary>
+    //    /// <param name="arguments"></param>
+    //    public override void Process(CreateArgs arguments)
+    //    {
+    //        Assert.ArgumentNotNull(arguments, "arguments");
+    //        System.Web.HttpRequest request = arguments.Context.HttpContext.Request;
+    //        if (request.Form.AllKeys.Length == 0)
+    //        {
+    //            return;
+    //        }
+    //        UpdateArgs updateArgs = new UpdateArgs(arguments.Scope);
+    //        CorePipeline.Run("itemWebApiUpdate", updateArgs);
+    //        arguments.Result = updateArgs.Result;
+    //    }
+    //}
 }
